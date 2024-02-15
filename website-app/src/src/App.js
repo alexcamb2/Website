8b6d46c297cd3986a9e743601f8aad80b2ed164c
alexcamb2 <alexcamb2@gmail.com>
2024-02-14 21:28:20 -0600

    First Commit
    First Commit

---
+++ website-app/src/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './Components/NavBar';
+import { Nav } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
